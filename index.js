4469d14b0940837ac6a4ef5fbd51e9cd7c436a05
= <=>
2015-12-16 17:31:10 -0600

    initial checkin

---
+++ index.js
@@ -0,0 +1,80 @@
+var config  = require('nconf');
+config.argv().env().file({file: __dirname + '/config.json'});
+
+var async           = require('async');
+var stream          = require('stream');
+var fs              = require('fs');
+var express         = require('express');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var util            = require('util');
+var multiparty      = require('multiparty');
+var fs              = require('fs');
+var template        = require('swig');
+var sha1            = require('node-sha1');
+var moment          = require('moment');
+var request         = require('request');
+
+var util            = require('./util');
+var models          = require('./models');
+var sequelize       = models.sequelize;
+var BirthdayCard    = models.BirthdayCard;
+
+template.setDefaults({
+    cache: false,   // JL DEBUG ~ turn off template caching.
+    autoescape: false
+});
+
+
+var app = express();
+app.set('port', (process.env.PORT || 80));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+app.use(express.static(__dirname + '/public'));
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+app.get('/', function(req, res) {
+    var tmpl = template.compileFile('templates/index.html');
+    res.send(tmpl({}));
+});
+
+app.post('/happy_birthday', function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    var form = new multiparty.Form();
+    form.parse(req, function(err, fields, files) {
+    	
+    	var data = {};
+		
+		for (var field in fields)
+            if (fields.hasOwnProperty(field))
+                data[field] = fields[field][0];
+        
+        console.log('got data:', data);
+        
+        console.log('creating transaction...');
+
+        // If we made it this far, we can save the artist to database
+        return sequelize.transaction().then(function (t) {
+
+            console.log('create birthday card...');
+
+            return BirthdayCard.create(data, {transaction: t}).then(function(u) {
+            	console.log('saved!');
+            	t.commit();
+            	return res.json({hooray: true});
+            }).catch(function(err) {
+                console.log('omfg error wtf: ', err);
+                t.rollback();
+                return error(err);
+            });
+        });
+    });
+ 
+});
---
+++ js_frontend/main.js
@@ -0,0 +1,96 @@
+var BirthdayController = Composer.Controller.extend({
+	elements: {
+		'ul': 'designs',
+		'textarea': 'message',
+		'span#remaining': 'remaining',
+		'input[name="name"]': 'name',
+		'input[name="address"]': 'address',
+		'input[name="city"]': 'city',
+		'input[name="state"]': 'state',
+		'input[name="zip"]': 'zip',
+		'input[name="email"]': 'email',
+		'input[name="opt_in"]': 'opt_in',
+	},
+	events: {
+		'click li a': 'switchDesign',
+		'keyup textarea': 'editMessage',
+		'change textarea': 'editMessage',
+		'submit form': 'submit'
+	},
+	
+	selectedCard: 'card1',
+	
+	switchDesign: function(e) {
+		if (e)
+			e.preventDefault();
+		
+		for (var i = 0; i < this.designs.childNodes.length; i++)
+			this.designs.childNodes[i].className = '';
+		
+		var target = e.target.parentNode.parentNode;
+
+		target.className = 'sel';
+		this.selectedCard = target.id;
+	},
+	
+	editMessage: function(e) {
+		var remaining = 350 - this.message.value.length;
+		this.remaining.textContent = remaining + ' characters remaining';
+		
+	},
+	
+	submit: function(e) {
+        e.preventDefault();
+
+        var error = false;
+
+        var add_error = function(el) {
+            el.className = 'error';
+            error = true;
+        };
+        
+        var required = ['name', 'address', 'city', 'state', 'zip', 'message']
+        
+        for (var i = 0; i < required.length; i++)
+        	if (!this[required[i]].value)
+        		add_error(this[required[i]]);
+
+        if (error) return alert('Please fill out all required fields :)');
+        
+        var formData = new FormData();
+        
+        for (var i = 0; i < required.length; i++)
+        	formData.append(required[i], this[required[i]].value);
+		
+		if (this.email.value)
+			formData.append('email', this.email.value);
+		
+		if (this.opt_in.checked)
+			formData.append('opt_in', 1);
+		
+		formData.append('design', this.selectedCard);
+
+        var url = '/happy_birthday';
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                console.log('response:', xhr.response);
+            }
+        }.bind(this);
+        xhr.open("post", url, true);
+        xhr.send(formData);
+        
+        new ShareModalController({
+        	headline: 'Thanks—we\'ll send your card!',
+        	text: 'Please share this page, and consider making a small donation to help cover the cost of printing and postage! :)'
+        });
+        this.el.style.display = 'none';
+        document.getElementById('share').style.display = 'inline';
+        
+        return false;
+    },
+})
+
+new BirthdayController({el: '#amibeingdetained'});
+window.DONATE_URL = 'https://donate.fightforthefuture.org/?tag=chelseabirthday';
